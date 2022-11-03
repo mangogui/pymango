8aa310b973750d5ff180abb93dc428f8e555bbb1
Parham Oyan <parhamoyan@yahoo.com>
2022-11-04 00:19:41 +0100

    code clean up

--- CocoaApplication.py
+++ CocoaApplication.py
@@ -1,4 +1,4 @@
-import ctypes
+from ctypes import c_void_p
 
 from Lib import LibMango
 
@@ -6,10 +6,10 @@ from Lib import LibMango
 class CocoaApplication(object):
     def __init__(self):
         LibMango.CocoaApplication_create.argtypes = []
-        LibMango.CocoaApplication_create.restype = ctypes.c_void_p
+        LibMango.CocoaApplication_create.restype = c_void_p
 
-        LibMango.CocoaApplication_run.argtypes = [ctypes.c_void_p]
-        LibMango.CocoaApplication_run.restype = ctypes.c_void_p
+        LibMango.CocoaApplication_run.argtypes = [c_void_p]
+        LibMango.CocoaApplication_run.restype = c_void_p
 
         self.obj = LibMango.CocoaApplication_create()
 
